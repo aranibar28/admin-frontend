2859923bac08a99d1c0e382dc4713ade562974be
Gerson Aranibar <aranibargerson28@gmail.com>
2022-04-10 23:50:55 -0500

    implements google auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -7,11 +12,76 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.renderButton();
+  }
+
+  public expression: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  public formSubmitted = false;
+  public auth2: any;
+
+  public loginForm = this.fb.group({
+    email: [localStorage.getItem('email') || 'xtest05@gmail.com', [Validators.required, Validators.pattern(this.expression)]],
+    password: ['123456', [Validators.required]],
+    remember: [false],
+  });
 
   login() {
-    this.router.navigateByUrl('/')
+    this.userService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        console.log(resp);
+        if (this.loginForm.get('remember')?.value) {
+          localStorage.setItem('email', this.loginForm.get('email')?.value);
+        } else {
+          localStorage.removeItem('email');
+        }
+        // Navegar al Dashboard
+        this.router.navigateByUrl('/');
+      },
+      (err) => {
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
+  }
+
+  // Rederizar el botón de Google
+  renderButton() {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+    });
+    this.startApp();
+  }
+
+  // Iniciar Sesión
+  startApp() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '589211754571-agupdbke0r35mun9p8o9ag10336s7uid.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+      });
+      this.attachSignin(document.getElementById('my-signin2'));
+    });
+  }
+
+  // Capturar el evento y enviar la respuesta
+  attachSignin(element: any) {
+    this.auth2.attachClickHandler(element, {}, (googleUser: any) => {
+        const id_token = googleUser.getAuthResponse().id_token;
+        this.userService.loginGoogle(id_token).subscribe();
+      },
+      (error: any) => {
+        alert(JSON.stringify(error, undefined, 2));
+      }
+    );
   }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  constructor(private fb: FormBuilder, private userService: UserService) {}
 
-  constructor() { }
+  public expression: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group(
+    {
+      name: ['Gerson Aranibar', [Validators.required, Validators.minLength(3)]],
+      email: ['xtest01@gmail.com', [Validators.required, Validators.pattern(this.expression)]],
+      password: ['123456', [Validators.required]],
+      password2: ['123456', [Validators.required]],
+      terms: [false, [Validators.required]],
+    },
+    {
+      validators: this.samePassword('password', 'password2'),
+    }
+  );
+
+  createUser() {
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // Realizar POST
+    this.userService.createUser(this.registerForm.value).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        Swal.fire("Error", err.error.msg, 'error')
+      }
+    );
   }
 
+  invalidFied(data: string): boolean {
+    if (this.registerForm.get(data)?.invalid && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  invalidPassword() {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if (pass1 !== pass2 && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  samePassword(pass1: string, pass2: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Controll = formGroup.get(pass1);
+      const pass2Controll = formGroup.get(pass2);
+
+      if (pass1Controll?.value === pass2Controll?.value) {
+        pass2Controll?.setErrors(null);
+      } else {
+        pass2Controll?.setErrors({ noEsIgual: true });
+      }
+    };
+  }
+
+  acceptTerms() {
+    return !this.registerForm.get('terms')?.value && this.formSubmitted;
+  }
 }
